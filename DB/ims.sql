febd8db067991152a7066a8ad4fa8d7f0c7b6735
DasunNethsara-04 <techsaralk.pro@gmail.com>
2022-11-21 07:09:16 -0800

    Login functionalities with updated database file

--- DB/ims.sql
+++ DB/ims.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2022 at 12:42 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Nov 21, 2022 at 04:03 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,89 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `ims`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `attendance`
---
-
-CREATE TABLE `attendance` (
-  `id` varchar(20) NOT NULL,
-  `studentId` varchar(20) NOT NULL,
-  `classId` varchar(20) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `classes`
---
-
-CREATE TABLE `classes` (
-  `id` varchar(20) NOT NULL,
-  `subjectId` varchar(20) NOT NULL,
-  `teacherId` varchar(20) NOT NULL,
-  `payment` int(4) NOT NULL,
-  `day` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `exams`
---
-
-CREATE TABLE `exams` (
-  `id` varchar(20) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `classId` varchar(20) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE `login` (
-  `id` varchar(20) NOT NULL,
-  `user` varchar(20) NOT NULL,
-  `pass` varchar(30) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  `lastLogin` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `payments`
---
-
-CREATE TABLE `payments` (
-  `id` varchar(20) NOT NULL,
-  `studentId` varchar(20) NOT NULL,
-  `classId` varchar(20) NOT NULL,
-  `year` varchar(4) NOT NULL,
-  `month` varchar(2) NOT NULL,
-  `status` int(1) NOT NULL,
-  `payment` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `results`
---
-
-CREATE TABLE `results` (
-  `id` varchar(20) NOT NULL,
-  `examId` varchar(20) NOT NULL,
-  `studentId` varchar(20) NOT NULL,
-  `marks` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -122,78 +39,14 @@ CREATE TABLE `students` (
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `subjects`
---
-
-CREATE TABLE `subjects` (
-  `id` varchar(20) NOT NULL,
-  `grade` int(2) NOT NULL,
-  `subject` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teachers`
---
-
-CREATE TABLE `teachers` (
-  `id` varchar(20) NOT NULL,
-  `name` varchar(40) NOT NULL,
-  `nic` varchar(20) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `telegramId` varchar(20) NOT NULL,
-  `contact` varchar(10) NOT NULL,
-  `status` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `classes`
---
-ALTER TABLE `classes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `exams`
---
-ALTER TABLE `exams`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `results`
---
-ALTER TABLE `results`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `subjects`
---
-ALTER TABLE `subjects`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `teachers`
---
-ALTER TABLE `teachers`
   ADD PRIMARY KEY (`id`);
 COMMIT;
 
